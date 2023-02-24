3094315b96d2fa9fa784cebc323ce11bebe0ce55
Zach W <64103243+Tuthorn@users.noreply.github.com>
2023-02-24 13:42:40 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import NavigationBar from './components/NavigationBar';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+  useLocation,
+} from "react-router-dom";
+import Login from './container/Login/Login';
+import Landing from './pages/Landing';
+import FuelHistory from './pages/FuelHistory';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
+import Userfront from "@userfront/react";
+
+Userfront.init("demo1234");
+
+function App() {
+  return (
+ <>
+<Router>
+<NavigationBar/>
+<Routes>
+        <Route exact path='/Landing' element={<Landing />} />
+        <Route path='/Login' element={<Login/>} />
+        <Route path='/Register' element={<Register/>} />
+        <Route path='/FuelHistory' element={<FuelHistory/>} />
+        {/* Replace the Dashboard with the user profile */}
+        <Route path="/Profile" element={<Profile />} />
+        <Route
+            path="/Dashboard"
+            element={
+              <RequireAuth>
+                <Profile />
+              </RequireAuth>
+            }      
+            />
+    </Routes>
+</Router>
+ </>
+  );
+}
+
+function RequireAuth({ children }) {
+  let location = useLocation();
+  if (!Userfront.tokens.accessToken) {
+    // Redirect to the /login page
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return children;
+}
+
+{/* Delete if replaced with user profile */}
+function Dashboard() {
+  const userData = JSON.stringify(Userfront.user, null, 2);
+ return (
+   <div>
+      <h2>Dashboard</h2>
+     <pre>{userData}</pre>
+      <button onClick={Userfront.logout}>Logout</button>
+    </div>
+ );
+}
+export default App;
