df001c78b71d0356a16797789dcca998d5d12307
daniellonda <78678458+daniellonda@users.noreply.github.com>
2023-02-24 03:07:49 -0600

    hopefully this works

---
+++ src/pages/FuelHistory.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Landing = () => {
+return (
+	<div>
+	<h1>
+		Gallons Requested:f
+	</h1>
+    <h1>
+        Delivery Address:
+	</h1>
+    <h1>
+        Delivery Date:
+	</h1>
+    <h1>
+        Price Per Gallon:
+    </h1>
+    <h1>
+        Total:
+    </h1>
+	</div>
+);
+};
+
+export default Landing;
